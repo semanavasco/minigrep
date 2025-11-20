6fd77fdd35582c7182ba0c8a1ea92b85a875d892
semanavasco <semanavasco@gmail.com>
2025-11-20 15:49:50 +0100

    Removed useless print

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,9 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
     if let Err(err) = minigrep::run(config) {
         println!("Application error: {err}");
         process::exit(1);
     }
 }
+
