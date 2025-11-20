55aebb88005b089ac15bbb70a059020475bb0660
semanavasco <semanavasco@gmail.com>
2025-11-20 14:55:38 +0100

    Returning errors from the run function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, process};
+use std::{env, error::Error, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,14 +11,18 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    run(config);
+    if let Err(err) = run(config) {
+        println!("Application error: {err}");
+        process::exit(1);
+    }
 }
 
-fn run(config: Config) {
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
